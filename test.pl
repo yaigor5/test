836ae27dc6efc027a936d9769968724f3cdf3737
root <iyarovoy@gmail.com>
2023-05-25 18:03:08 +0300

    init

---
+++ test.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+##################################
+### Written by IGOR YAROVOY   ####
+##################################
+BEGIN { unshift(@INC, '/var/www/cgi-bin/lib'); }
+use 5.10.1;
+use DBI;
+use CGI;
+use CGI::Carp qw(fatalsToBrowser); # Если будет какая-нибудь ошибка, то выведет в броузер
+use utf8;
+use Time::localtime; 
+use Data::Dumper;
+$|=1; ## запрещаем буферизацию вывода. 
+###INIT
+
+our $VERSION = '2023-05-26';
+
+####### script name
+my $actionscriptname = $0;
+$actionscriptname =~ s/.*\/www(.*)/$1/g; ## /www/cgi-bin/...
+$actionscriptname =~ /([^\/]+\..*)/g;
+my $scriptname = $1;
+
+print "Content-type: text/html\n\n";
+
+print $scriptname."<br>\n";
