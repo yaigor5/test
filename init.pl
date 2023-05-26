3401b168c7a3d2eabec83074dbcdcfad612e5cfe
jia <yaigor@mail.ru>
2023-05-26 16:42:36 +0300

    test

--- init.pl
+++ init.pl
@@ -32,8 +32,8 @@ eval {
         }
     }
     # проверка доступности MySQL сервера
-    eval "use Config::IniFiles";
-    eval "use DBI";
+    #eval "use Config::IniFiles";
+    #eval "use DBI";
     my $config = Config::IniFiles->new(-file => 'config.ini') or die "Не удалось открыть файл config.ini: $!";
     my $mysql_host = $config->val('database', 'host');
     my $mysql_port = $config->val('database', 'port');
