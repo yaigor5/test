4868168077f853a65822a39f8fb0e9d1526fa3e1
jia <yaigor@mail.ru>
2023-05-30 15:48:07 +0300

    test

--- test.pl
+++ test.pl
@@ -129,15 +129,13 @@ __DATA__
 
             <% if (stash('messages')) { %>
                 <% foreach my $message (@{stash('messages')}) { %>
-                    <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-                        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="6000" <% if (!$message->{nohide}) { %>data-autohide="false"<% } %>>
-                            <div class="toast-header <%= $message->{type} %> text-white">
-                                <strong class="me-auto"><%= $message->{title} %></strong>
-                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                            </div>
-                            <div class="toast-body">
-                                <%= $message->{content} %>
-                            </div>
+                    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="6000" <% if (!$message->{nohide}) { %>data-autohide="false"<% } %>>
+                        <div class="toast-header <%= $message->{type} %> text-white">
+                            <strong class="me-auto"><%= $message->{title} %></strong>
+                            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                        <div class="toast-body">
+                            <%= $message->{content} %>
                         </div>
                     </div>
                 <% } %>
