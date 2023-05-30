63e16bb5b53ff937e81f92bfba03cff6bc3167e1
jia <yaigor@mail.ru>
2023-05-30 10:58:21 +0300

    fix

--- test.pl
+++ test.pl
@@ -21,15 +21,23 @@ Lib1::log_parser();
 ## вывод основного содержимого на экран
 get '/' => sub {
     my $c = shift;
-    my $sth = $dbh->prepare("SELECT * FROM `message`");
-    $sth->execute();
+    my $search_text = $c->param('search_text');
+
+    my $sth;
+    if ($search_text) {
+        $sth = $dbh->prepare("SELECT * FROM `message` WHERE `str` LIKE ?");
+        $sth->execute("%$search_text%");
+    }
+
     my @results;
     while (my $row = $sth->fetchrow_hashref) {
         push @results, $row;
     }
     $c->stash(results => \@results);
+
     $c->render('index');
 };
+
 
 ## запуск mojo
 app->start;
@@ -43,24 +51,47 @@ __DATA__
 <head>
     <title>Вывод</title>
     <meta charset="utf-8">
-    <!-- <link rel="stylesheet" type="text/css" href="/css/style.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <% if (stash('results')) { %>
-        <table style='border: 1;'>
-            <thead>
-                <tr>
-                    <th>created</th><th>int_id</th><th>str</th>
-                </tr>
-            </thead>
-            <tbody>
-                <% foreach my $row (@{stash('results')}) { %>
-                    <tr>
-                        <td><%= $row->{created} %></td><td><%= $row->{int_id} %></td><td><%= $row->{str} %></td>
-                    </tr>
-                <% } %>
-            </tbody>
-        </table>
-    <% } %>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col">
+                <form method="GET" action="/">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Введите текст" name="search_text">
+                        <button class="btn btn-primary" type="submit">Поиск</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <% if (stash('results')) { %>
+            <div class="row">
+                <div class="col">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>created</th>
+                                <th>int_id</th>
+                                <th>str</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <% foreach my $row (@{stash('results')}) { %>
+                                <tr>
+                                    <td><%= $row->{created} %></td>
+                                    <td><%= $row->{int_id} %></td>
+                                    <td><%= $row->{str} %></td>
+                                </tr>
+                            <% } %>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        <% } %>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
